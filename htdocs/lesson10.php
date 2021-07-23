ed5a6ca18fd6b4a0781192c3fe13a163bc2f66cb
zunema <65650872+zunema@users.noreply.github.com>
2021-07-23 23:29:26 +0900

    fix

--- htdocs/lesson10.php
+++ htdocs/lesson10.php
@@ -2,13 +2,13 @@
 $score = array(10, 60, 90, 70, 50);
 foreach ($score as $key => $value) {
   if ($value > 80) {
-    echo "${value}点は「優」です。"."\n";
+    echo "${value}点は「優」です。"."<br>";
   } elseif ($value > 60) {
-    echo "${value}点は「良」です。"."\n";
+    echo "${value}点は「良」です。"."<br>";
   } elseif ($value > 40) {
-    echo "${value}点は「可」です。"."\n";
+    echo "${value}点は「可」です。"."<br>";
   } else {
-    echo "${value}点は「不可」です。"."\n";
+    echo "${value}点は「不可」です。"."<br>";
   }
 }
 
