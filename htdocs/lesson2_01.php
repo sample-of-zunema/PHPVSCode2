7085f3a7fb8b98bb19077378a3f78b15a6be373d
zunema <65650872+zunema@users.noreply.github.com>
2021-07-23 23:52:03 +0900

    fix

--- htdocs/lesson2_01.php
+++ htdocs/lesson2_01.php
@@ -1,13 +1,11 @@
 <?php
-for ($i = 0; $i < 330; $i++) {
-  if ($i == 1){
-    echo $i."\n";
-  } elseif ($i == 21) {
-    echo $i."\n";
-  } elseif ($i == 321) {
-    echo $i."\n";
+for ($i=2; $i>=0; $i--) {
+  for ($j=3; $j>=$i+1; $j--) {
+    echo $j-$i;
   }
+  echo "<br>";
 }
+
 // 以下をfor文を使用して表示してください。
 
 // 1
