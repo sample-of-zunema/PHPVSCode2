d811e739afc12ca65fde80f1c943999d17f3ae8b
zunema <65650872+zunema@users.noreply.github.com>
2021-07-24 00:02:08 +0900

    fix

--- htdocs/lesson2_02.php
+++ htdocs/lesson2_02.php
@@ -1,13 +1,13 @@
 <?php
-for ($i = 0; $i < 800; $i++) {
-  if ($i == 123){
-    echo $i."\n";
-  } elseif ($i == 456) {
-    echo $i."\n";
-  } elseif ($i == 789) {
-    echo $i."\n";
+$i = 1;
+for ($j=1; $j<=3; $j++) {
+  for ($k=1; $k<=3; $k++) {
+    echo $i;
+    $i++;
   }
+  echo "<br>";
 }
+
 // 以下をfor文を使用して表示してください。
 
 // 123
